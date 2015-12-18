a493ff3514725ded4dcc6ca5bdcc5e1813d1c1c8
bliutwo <ossicle2@gmail.com>
2015-12-18 01:58:07 -0800

    Finished LongestCollatzSequence.

---
+++ LongestCollatzSequence/LongestCollatzSeq.py
@@ -0,0 +1,34 @@
+# File: LongestCollatzSeq.py
+# Description: determines what starting number, under one million, produces
+#              the longest Collatz sequence
+
+def printList(sampleList):
+    print '[%s]' % ', '.join(map(str, sampleList))
+
+def addToSeq(num, collatzSeq):
+    collatzSeq.append(num)
+    if num == 1:
+        return
+    elif num % 2 == 0:
+        addToSeq((num/2), collatzSeq)
+    else:
+        addToSeq(((3*num) + 1), collatzSeq)
+
+def produceCollatzSeq(num):
+    collatzSeq = []
+    collatzSeq.append(num)
+    addToSeq(num, collatzSeq)
+    # printList(collatzSeq)
+    return collatzSeq
+
+def main():
+    listOfLengths = []
+    for num in range(1, 1000000):
+        collatzSeq = produceCollatzSeq(num)
+        listOfLengths.append(len(collatzSeq))
+        percent = num / 1000000.0
+        print 'percent: %f' % percent
+    print 'maximum length with starting number: %d' % (listOfLengths.index(max(listOfLengths)) + 1)
+
+if __name__ == "__main__":
+    main()
