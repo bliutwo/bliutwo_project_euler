f22ead3d635821c5989db721584daf1b7ea82bc2
bliutwo <ossicle2@gmail.com>
2015-12-18 02:30:53 -0800

    Added LatticePaths and its corresponding files. Going to continue working on the rest of these problems tomorrow.

---
+++ LatticePaths/LatticePaths.c
@@ -0,0 +1,10 @@
+// File: LatticePaths.c
+// Description: In theory, I only need ints to solve this problem.
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("Hello world!\n");
+    return 0;
+}
