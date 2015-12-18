3a7b6cb44360a981d45e81277ca0d8f746dbbaaa
bliutwo <ossicle2@gmail.com>
2015-12-18 11:04:39 -0800

    Added function to determine number of paths.

--- LatticePaths/LatticePaths.c
+++ LatticePaths/LatticePaths.c
@@ -3,6 +3,12 @@
 
 #include <stdio.h>
 
+int solvePaths(int num)
+{
+
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
     int num = 0;
@@ -11,6 +17,10 @@ int main(int argc, char* argv[])
     scanf("%d", &num);
 
     printf("This is num that you inputted: %d\n", num);
+
+    int paths = solvePaths(num);
+    
+    printf("Number of routes in a %dx%d grid: %d\n", num, num, paths);
 
     return 0;
 }
