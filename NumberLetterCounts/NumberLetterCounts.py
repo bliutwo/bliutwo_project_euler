db65dfdcb25e00adcb8718120ee324e4507f8524
bliutwo <ossicle2@gmail.com>
2015-12-19 02:36:59 -0800

    Added NumberLetterCounts

---
+++ NumberLetterCounts/NumberLetterCounts.py
@@ -0,0 +1,8 @@
+# Filename: NumberLetterCounts.py
+# Description: Counts the number of letters used for numbers from 1 to
+#              1000 
+
+def main():
+
+if __name__ == "__main__":
+    main()
