2eb8ff90bd52d4fb74d12b740309ed271e77cfec
bliutwo <ossicle2@gmail.com>
2015-12-20 15:54:25 -0800

    Did nonabundantsums.py, but it takes forever. Why?

--- Non-abundantSums/nonabundantsums.py
+++ Non-abundantSums/nonabundantsums.py
@@ -5,11 +5,14 @@
 # the sum of two abundant numbers.
 # Abundant number: sum of its proper divisors exceeds 'number'
 
+# TODO: this program takes forever to run. Why?
+
 import sys
 sys.dont_write_bytecode = True
 sys.path.insert(0, '../AmicableNumbers/')
 from AmicableNumbers import *
 # sumOfProperDivisors(num)
+# sumOfElements(samplelist)
 
 def isAbundant(num):
     sumProperDivisors = sumOfProperDivisors(num)
@@ -21,8 +24,6 @@ def isAbundant(num):
 # how do you find out if a number can be written as the sum of two
 # abundant numbers?
 # the two numbers that are in the sum must be less than num
-def cantSumAbundant(num, listOfAbundantNumbers):
-    return True
 
 def getAllAbundantNumbers(small, large):
     listOfAbundantNumbers = []
@@ -30,6 +31,14 @@ def getAllAbundantNumbers(small, large):
         if isAbundant(num):
             listOfAbundantNumbers.append(num)
     return listOfAbundantNumbers
+
+def getAllAbundantNumbersSums(listOfAbundantNumbers):
+    listOfAbundantNumbersSums = []
+    for num in listOfAbundantNumbers:
+        for snum in listOfAbundantNumbers:
+            singleSum = num + snum
+            listOfAbundantNumbersSums.append(singleSum)
+    return listOfAbundantNumbersSums
     
 # the smallest number that can be written as the sum of two
 # abundant nums is 24
@@ -39,15 +48,17 @@ def main():
     totalSum = 0
     # 12 is the smallest abundant number
     listOfAbundantNumbers = getAllAbundantNumbers(12, 28124)
-    for num in xrange(24,28124):
-        if cantSumAbundant(num, listOfAbundantNumbers):
-            totalSum+=num
-    print "The sum of all positive integers which cannot be written as the sum of two abundant numbers is: %d" % totalSum
-    for num in xrange(1,20):
-        if isAbundant(num):
-            print "%d is abundant" % num
-        else: 
-            print "%d is deficient" % num
+    print "got list of abundant numbers"
+    listOfPositiveIntegers = []
+    for num in xrange(24, 28124):
+        listOfPositiveIntegers.append(num)
+    listOfAbundantNumbersSums = getAllAbundantNumbersSums(listOfAbundantNumbers)
+    print "got list of abundant numbers sums"
+    for num in listOfAbundantNumbersSums:
+        if num in listOfPositiveIntegers:
+            listOfPositiveIntegers.remove(num)
+    print "filtered list of positive integers"
+    print "The sum of all positive integers which cannot be written as the sum of two abundant numbers is: %d" %  sumOfElements(listOfPositiveIntegers)
 
 if __name__ == "__main__":
     main()
