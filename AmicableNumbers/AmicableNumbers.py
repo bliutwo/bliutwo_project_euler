43563f02181ffbf2f4fdad9367d1471cc2ba465b
bliutwo <ossicle2@gmail.com>
2015-12-20 01:26:42 -0800

    Just deleted all print statements.

--- AmicableNumbers/AmicableNumbers.py
+++ AmicableNumbers/AmicableNumbers.py
@@ -20,32 +20,22 @@ def getProperDivisors(num):
     for potentialDivisor in range(1, (num/2)+1):
         if num % potentialDivisor == 0:
             listOfProperDivisors.append(potentialDivisor)
-    # printList(listOfProperDivisors)
     return listOfProperDivisors
 
 def sumOfProperDivisors(num):
     listOfProperDivisors = getProperDivisors(num)
     total = sumOfElements(listOfProperDivisors)
-    # print total
     return total
 
 def getAllAmicableNumbers(upperBound):
     listOfAmicableNumbers = []
     for potentialAmicable in range(1, upperBound):
-        # print "potentialAmicable: %d" % potentialAmicable
         if potentialAmicable not in listOfAmicableNumbers:
             secondPotentialAmicable = sumOfProperDivisors(potentialAmicable)
-            # print "secondPotentialAmicable: %d" % secondPotentialAmicable
             sumSecondDivisors = sumOfProperDivisors(secondPotentialAmicable)
-            # print "sumSecondDivisors: %d" % sumSecondDivisors
             if (sumSecondDivisors is potentialAmicable) and (secondPotentialAmicable is not potentialAmicable):
                 listOfAmicableNumbers.append(potentialAmicable)
                 listOfAmicableNumbers.append(secondPotentialAmicable)
-                # print "YES"
-                # print "amicable1: %d" % potentialAmicable
-                # printList(getProperDivisors(potentialAmicable))
-                # print "amicable2: %d" % secondPotentialAmicable
-                # printList(getProperDivisors(secondPotentialAmicable))
     return listOfAmicableNumbers
 
 def main():
