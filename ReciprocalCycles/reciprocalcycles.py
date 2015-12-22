162b9129577cfc431ea922b1b2cf54fe87b517fa
bliutwo <ossicle2@gmail.com>
2015-12-22 13:34:59 -0800

    Added reciprocalcycles.py and thoughts.txt

---
+++ ReciprocalCycles/reciprocalcycles.py
@@ -0,0 +1,10 @@
+# Filename: reciprocalcycles.py
+# Description: https://projecteuler.net/problem=26
+
+def main():
+    print "Hello world!"
+    # How do I parse a fraction such that I know if it has a recurring
+    # cycle and how long the recurring cycle is
+
+if __name__ == "__main__":
+    main()
