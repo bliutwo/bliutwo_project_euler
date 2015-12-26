627c9e95329af87d4639b5c9a60919f152a557bf
bliutwo <ossicle2@gmail.com>
2015-12-25 19:50:35 -0800

    Barely started pandigitalmultiples.c

---
+++ PandigitalMultiples/pandigitalmultiples.c
@@ -0,0 +1,10 @@
+// Filename: pandigitalmultiples.c
+// Description: https://projecteuler.net/problem=38
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	printf("Hello world!\n");
+	return 0;
+}
