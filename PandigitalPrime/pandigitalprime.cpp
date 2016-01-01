3c7d303f62a9dab99b86ac67684fec4f51c5fa22
bliutwo <ossicle2@gmail.com>
2016-01-01 03:57:25 -0800

    Wrote main for pandigitalprime, but need to implement finding the largest pandigital prime given digits.

---
+++ PandigitalPrime/pandigitalprime.cpp
@@ -0,0 +1,38 @@
+// Filename: pandigitalprime.cpp
+// Description: https://projecteuler.net/problem=41
+
+#include <iostream>
+
+// TODO: implement this
+int getLargestPandigitalPrime(int digits)
+{
+
+	return -1;
+}
+
+int main()
+{
+	// how to generate a pandigital?
+	// how to know at what n to stop? - after x generated "no primes," stop
+	int n = 1;
+	int consecNoPrimes = 0;
+	int panDigitalPrime = getLargestPandigitalPrime(n);
+	int digits = n;
+	while(consecNoPrimes < 10)
+	{
+		int nextPanDigPrime = getLargestPandigitalPrime(n+1);
+		if (nextPanDigPrime == -1)
+		{
+			consecNoPrimes++;
+		} else {
+			panDigitalPrime = nextPanDigPrime;
+			digits = n+1;
+			consecNoPrimes = 0;
+		}
+		n++;
+	}
+	std::cout << "The largest n-digit pandigital prime that exists has ";
+	std::cout << digits << " digits and is " << panDigitalPrime;
+	std::cout << std::endl;
+	return 0;
+}
