f0dd66f48b4a5ec6be6e9f84e2a64a2a04c1e488
bliutwo <bliutwo@cs.stanford.edu>
2017-07-04 16:32:08 -0700

    Finish down to right, fix first line grid
    Diag next

--- LargestProdInGrid/largestprodingrid.py
+++ LargestProdInGrid/largestprodingrid.py
@@ -1,16 +1,35 @@
 # Filename: largestprodingrid.py
 # Description: Returns the largest product in a grid.
 
-# TODO: right algorithm ok
-# TODO: check accuracy of down
-# TODO: convert grid to downgrid, then use right algorithm
+# TODO: right algorithm ok.. DONE!
+# TODO: check accuracy of down.. DONE!
+# TODO: convert grid to downgrid, then use right algorithm.. DONE!
 # TODO: the rest of it, mainly diagonal
 
 import sys
 sys.dont_write_bytecode = True
 
 import argparse
-from chemoUtils import getGrid
+
+# returns a grid with all information given an opened file of the csv file
+def getGrid(opened):
+    # use opened to obtain the list of pairs
+    # read first line of file since the first line is a "key/legend"
+    # opened.readline()
+    grid = []
+    for line in opened:
+        # parse line to get first part of pair
+        line = line.strip()
+        lineList = line.split(",")
+        grid.append(lineList)
+    return grid
+
+def print_grid(g):
+    for l in g:
+        s = ""
+        for w in l:
+            s = s + str(w) + " "
+        print s
 
 def get_greatest(intList):
     return max(intList)
@@ -32,10 +51,10 @@ def get_all_right(grid):
         lastElemIndex = len(row) - 1
 #        print len(row)
 #        print lastElemIndex
-        while (lastElemIndex - 1 > 0):
+        while (lastElemIndex - 2 > 0):
 #            print "we've entered the while loop"
             prodList = []
-            for i in range(0,3):
+            for i in range(0,4):
                 prodList.append(row[lastElemIndex - i])
 #            print prodList
 #            print row
@@ -46,42 +65,37 @@ def get_all_right(grid):
 #    print listOfRight
     return listOfRight
 
-def get_all_diag(grid):
-    allDiag = []
-    rowLength = len(grid[0])
-    colLength = len(grid)
-
-    print "in function get_all_diag()"
-    print allDiag
-    return allDiag
 
 # TODO
+def get_all_diag(grid):
+    allDiag = []
+    return allDiag
+
 def make_down_grid(g):
     dg = []
+    for i in range(0, len(g[0])):
+        col = []
+        for row in g:
+            col.append(row[i]) 
+        dg.append(col)
     return dg
 
 def get_largest_multiplicant(grid):
     allRight = get_all_right(grid)
-    print allRight
+#    print allRight
     dg = make_down_grid(grid)
+#    print_grid(grid)
+#    print "About to print dg"
+#    print_grid(dg)
     allDown  = get_all_right(dg)
-    print allDown
-
+#    print allDown
     assert(False)
-
     allDiag  = get_all_diag(grid)
     print allDiag
     allProds = allDown + allRight + allDiag
     print allProds
     greatestProduct = get_greatest(allProds)
     return greatestProduct
-
-def print_grid(g):
-    for l in g:
-        s = ""
-        for w in l:
-            s = s + str(w) + " "
-        print s
 
 def int_up_grid(sg):
     ig = []
